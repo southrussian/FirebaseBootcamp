26773999a3434be5a34572e4ff0a763d44cd48fc
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-04-16 13:48:43 +0300

    Initial Commit

---
+++ FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampApp.swift
+//  FirebaseBootcamp
+//
+//  Created by Danil Peregorodiev on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
